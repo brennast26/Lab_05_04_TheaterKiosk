0c405d3ffcabdac77f81c09824e292c40d0067cd
Brennast26 <brennast@mail.uc.edu>
2022-02-21 19:49:11 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args)
+    {
+	int ageinput = 18;
+    if (ageinput >= 21)
+        {
+        System.out.println("You are old enough to receive a wristband.");
+        }
+    else
+        {
+        System.out.println("Have a good day.");
+        }
+    }
+}
